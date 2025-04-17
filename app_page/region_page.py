b35e5acf0eb22d1e013f09760decbaacdc2d0be0
GrayMists <andrew.puliak@gmail.com>
2025-04-17 20:47:47 +0300

    додана інформація на стоірнку регіони

--- app_page/region_page.py
+++ app_page/region_page.py
@@ -11,4 +11,32 @@ from data_cleaner import change_district_name
 
 
 def show_data():
-    st.write("Hello")
+    # Перевірка, чи є дані
+    df = get_session_dataframe()
+    if df is not None:
+        #Переконуємось, що всі назви колонок є рядками, та очищаємо від пробілів на початку і вкінці
+        df.columns = df.columns.astype(str).str.strip()
+        
+        # Вибір регіону
+        if "Регіон" not in df.columns:
+            st.error("Для отримання даних треба ввести правильне посилання")
+            df["Регіон"] = df["Регіон"].apply(change_district_name)
+            return
+        else:
+            col1, col2 = st.columns(2)
+
+            with col1:
+                first_selected_region = st.selectbox("Оберіть регіон:", df["Регіон"].unique(), key="first_region")
+                first_filtered_df = prepare_filtered_data(df, first_selected_region)
+                st.write("ТОП-5 найбільших продаж області")
+                st.dataframe(first_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values(ascending=False).head(5))
+                st.write("ТОП-5 найменших продаж області")
+                st.dataframe(first_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values().head(5))
+
+            with col2:
+                second_selected_region = st.selectbox("Оберіть регіон:", df["Регіон"].unique(), key="second_region")
+                second_filtered_df = prepare_filtered_data(df, second_selected_region)
+                st.write("ТОП-5 найбільших продаж області")
+                st.dataframe(second_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values(ascending=False).head(5))
+                st.write("ТОП-5 найменших продаж області")
+                st.dataframe(second_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values().head(5))
