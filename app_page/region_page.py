46d4ac0af12201387748c594484ca2592752cda0
GrayMists <andrew.puliak@gmail.com>
2025-04-18 17:19:51 +0300

    створення додаткових колонок на сторінці регіонів

--- app_page/region_page.py
+++ app_page/region_page.py
@@ -28,15 +28,21 @@ def show_data():
             with col1:
                 first_selected_region = st.selectbox("Оберіть регіон:", df["Регіон"].unique(), key="first_region")
                 first_filtered_df = prepare_filtered_data(df, first_selected_region)
-                st.write("ТОП-5 найбільших продаж області")
-                st.dataframe(first_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values(ascending=False).head(5))
-                st.write("ТОП-5 найменших продаж області")
-                st.dataframe(first_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values().head(5))
+                col3, col4 = st.columns(2)
+                with col3:
+                    st.write("ТОП-5 найбільших продаж області")
+                    st.dataframe(first_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values(ascending=False).head(5))
+                with col4:
+                    st.write("ТОП-5 найменших продаж області")
+                    st.dataframe(first_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values().head(5))
 
             with col2:
                 second_selected_region = st.selectbox("Оберіть регіон:", df["Регіон"].unique(), key="second_region")
                 second_filtered_df = prepare_filtered_data(df, second_selected_region)
-                st.write("ТОП-5 найбільших продаж області")
-                st.dataframe(second_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values(ascending=False).head(5))
-                st.write("ТОП-5 найменших продаж області")
-                st.dataframe(second_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values().head(5))
+                col5, col6 = st.columns(2)
+                with col5:
+                    st.write("ТОП-5 найбільших продаж області")
+                    st.dataframe(second_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values(ascending=False).head(5))
+                with col6:
+                    st.write("ТОП-5 найменших продаж області")
+                    st.dataframe(second_filtered_df.groupby("Найменування")["Кількість"].sum().sort_values().head(5))
