2c409b9fe610a6c36d6803e58f4afdd3d12d51c0
GrayMists <andrew.puliak@gmail.com>
2025-04-19 13:54:08 +0300

    меню і завантаження файлів винесено в sidebar

--- st.py
+++ st.py
@@ -13,36 +13,32 @@ import app_page.city_page as city_page
 
 from data_loader import load_data
 
-# Створюємо вибір між сторінками
-
-
-#with st.sidebar:
-selected = option_menu(
-    menu_title=None,
-    options=["Завантаження", "Регіони", "Місто"],
-    icons=["cloud-upload","back" ,"bar-chart"],
-    menu_icon="cast",
-    default_index=0,
-    orientation="horizontal",
-    key="main_menu" 
-)
-if selected == "Завантаження":
-    # Поле для введення посилання
-    sheet_url = st.text_input("Вставте посилання на Google Таблицю:")
-    uploaded_file = st.file_uploader("Оберіть файл")
-    load_button = st.button("Завантажити")
-
-    
-    # Ініціалізуємо порожній DataFrame
-    df = pd.DataFrame()
+with st.sidebar:
+    selected = option_menu(
+        menu_title=None,
+        options=["Завантаження", "Регіони", "Місто"],
+        icons=["cloud-upload","back" ,"bar-chart"],
+        menu_icon="cast",
+        default_index=0,
+        #orientation="horizontal",
+        key="main_menu" 
+    )
 
     if selected == "Завантаження":
+        sheet_url = st.text_input("Вставте посилання на Google Таблицю:")
+        uploaded_file = st.file_uploader("Оберіть файл")
+        load_button = st.button("Завантажити")
+
+        df = pd.DataFrame()
+
         if uploaded_file is not None:
             if uploaded_file.name.endswith((".xlsx", ".xls")):
                 try:
                     df = pd.read_excel(uploaded_file)
                     st.success("Файл успішно завантажено!")
                     df.columns = df.columns.str.replace(" ", "")  # Видалення пробілів
+                    excluded_columns = ["Adding", "ЄДРПОУ", "Юр.адресаклієнта"]
+                    df = df[df['Регіон'].isin(['24. Тернопіль', '10. Івано-Франк'])]
                     st.session_state.df = df
                 except Exception as e:
                     st.error(f"Помилка при зчитуванні файлу: {e}")
@@ -52,15 +48,15 @@ if selected == "Завантаження":
             df = load_data(sheet_url)
             if isinstance(df, str):
                 st.error(df)
-                df = pd.DataFrame()  # Скидаємо df у випадку помилки
+                df = pd.DataFrame()
             else:
                 st.success("Дані успішно завантажені з посилання!")
                 st.session_state.df = df
 
-    if not df.empty:
-        st.write("Колонки таблиці:", list(df.columns))
+        if not df.empty:
+            st.write("Колонки таблиці:", list(df.columns))
 
-elif selected == "Регіони":
+if selected == "Регіони":
     rerion_page.show_data()
 elif selected == "Місто":
     city_page.show_data()
