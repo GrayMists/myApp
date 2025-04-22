bf4678d4b20bb2278e41b7c9391aeeed80c45bbc
GrayMists <andrew.puliak@gmail.com>
2025-04-22 22:12:00 +0300

    видалено непотрібну функцію

--- app_page/city_page.py
+++ app_page/city_page.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from data_processing import (
-    get_session_dataframe, 
     prepare_filtered_data)
 
 from data_cleaner import change_district_name
