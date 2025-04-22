15154af6711c3bbcd45360c16402a7508283d533
GrayMists <andrew.puliak@gmail.com>
2025-04-22 19:41:38 +0300

    забираємо помилку мультиіндексу

--- app_page/city_page.py
+++ app_page/city_page.py
@@ -49,7 +49,7 @@ def show_data():
             pivot_ternopil_street = pd.pivot_table(
                 filtered_df,
                 values="Кількість",
-                index="Вулиця",
+                index=["Місто", "Вулиця"],
                 columns="Найменування",
                 aggfunc="sum"
             )
