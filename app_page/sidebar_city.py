ce920cab128da3d12643d310e436e6bce42c1d69
GrayMists <andrew.puliak@gmail.com>
2025-04-30 11:02:15 +0300

    виправлено помилку з state

--- app_page/sidebar_city.py
+++ app_page/sidebar_city.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+# Ініціалізація df, якщо він ще не існує
+if 'df' not in st.session_state:
+    st.session_state.df = pd.DataFrame()  # або ваш DataFrame
+
+# Тепер безпечно використовувати
 df = st.session_state.df
 def show_sidebar_data():
     with st.container(border=True):
